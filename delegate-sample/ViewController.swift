9891632c908ac96febbe88e52a7688656fd68297
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2023-01-12 18:13:45 +0900

    Initial Commit

---
+++ delegate-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegate-sample
+//
+//  Created by Yuta Shoji on 2023/01/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
